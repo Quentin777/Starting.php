051861826bc39a8da9f401690b999320978b1095
Quentin Grau <q.grau00@gmail.com>
2017-03-07 14:02:07 +0100

    tout

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Starting.php
+
+index.php (avec le html 5)
+
+Dans index.php un lien qui va pointer sur le fichier variable.php
+
+
+Dans ce variable.php
+
+Exercice 1 : 
+
+On crée une variable nom et on choisi son contenu
+
+
+Exercice 2 
+
+age nom prenom
+
+On crée trois variable 
+
+Exercice 3
+
+On crée une variable KM on va changer sa valeur par 3 et on affiche son comptenu
+et on va échanger sa valeur par 125 et on affiche son comptenu
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<h1>Exercice 1</h1>
+
+<?php  
+
+	$nom = "Jenesaispas";
+	echo "$nom";
+
+?>
+
+<h1>Exercice 2</h1>
+<?php  
+
+	$prenom = "Remi";
+	$nom = "sans-famille";
+	$age = "12";
+	echo "$prenom";
+	echo "$nom";
+	echo = "$age";
+
+?>
+
+<footer></footer>
+</body>
+</html>
