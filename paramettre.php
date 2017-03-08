6c220c245f2a111e500fff845b557900696a31d9
Quentin Grau <q.grau00@gmail.com>
2017-03-08 15:11:16 +0100

    tout

--- paramettre.php
+++ paramettre.php
@@ -9,6 +9,7 @@
 <a href="paramettre/exercice2.php?nom=Nemare&prenom=Jean"> exercice2 </a>
 <a href="paramettre/exercice3.php?dateDebut=2/05/2016&dateFin=27/11/2016"> exercice3 </a>
 <a href="paramettre/exercice4.php?langage=PHP&serveur=LAMP"> exercice4 </a>
-<a href="paramettre/exercice5.php"> exercice5 </a>
+<a href="paramettre/exercice5.php?semaine=12"> exercice5 </a>
+<a href="paramettre/exercice6.php?batiment=12&salle=101"> exercice6 </a>
 </body>
 </html>
