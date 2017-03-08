150f7b2bfb9a146062070c7530d7fd8e88dce2e9
Quentin Grau <q.grau00@gmail.com>
2017-03-08 10:34:15 +0100

    tout

--- boucle.php
+++ boucle.php
@@ -14,11 +14,46 @@
 
 ?>
 
+<?php  
+	$varun = 0;
+	$vardeux = 3;
+
+	while ($varun <= 20){
+		$varun = $varun * $vardeux;
+		echo "$varun.</br>";
+    	$varun = $varun + 1;
+    }
+
+?>
+
+
+<?php  
+	
+	$varun = 100;
+	$vardeux = 1;
+
+	while ($varun > 10){
+		$varun = $varun * $vardeux;
+		echo "$varun.</br>";
+		$varun = $varun - 50;
+	}
+
+?>
 
 <?php  
 
 	$test = "on arrive presque";
 	for ($i=0; $i<5 ; $i++){ 
+		echo "$test.<br>";
+	}
+
+
+?>
+
+<?php  
+
+	$test = "c'est presque bon";
+	for ($i=20; $i>0 ; $i--){ 
 		echo "$test.<br>";
 	}
 
