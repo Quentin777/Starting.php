2f9590100eba3589798afcc17c99de2ace3c8b06
Quentin Grau <q.grau00@gmail.com>
2017-03-08 16:09:13 +0100

    tout

--- formulaire.php
+++ formulaire.php
@@ -10,12 +10,25 @@
  <p>Votre nom : <input type="text" name="nom" /></p>
  <p>Votre âge : <input type="text" name="age" /></p>
  <p><input type="submit" value="OK"></p>
+  	<SELECT name="Civilité" size="1">
+	<OPTION>Mr
+	<OPTION>Mme
+	<OPTION>Mademoiselle
+	</SELECT>
 </form>
 
 <form action="user.php" method="POST">
  <p>Votre nom : <input type="text" name="nom" /></p>
  <p>Votre âge : <input type="text" name="age" /></p>
+ 	<SELECT name="Civilité" size="1">
+	<OPTION>Mr
+	<OPTION>Mme
+	<OPTION>Mademoiselle
+	</SELECT>
  <p><input type="submit" value="OK"></p>
 </form>
+
+
+
 </body>
 </html>
