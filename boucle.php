9a9114c9592d4c61621f961e2938ecab52631174
Quentin Grau <q.grau00@gmail.com>
2017-03-08 10:56:40 +0100

    tout

--- boucle.php
+++ boucle.php
@@ -52,6 +52,7 @@
 
 ?>
 
+<br>
 <?php  
 
 	$test = "c'est presque bon";
@@ -67,6 +68,17 @@
 
 	$test = "on tieng le bon boo";
 	for ($i=0; $i<100 ; $i=$i+15){ 
+		echo "$test.<br>";
+	}
+
+
+?>
+
+<br>
+<?php  
+
+	$test = "enfin";
+	for ($i=200; $i>0 ; $i=$i-12){ 
 		echo "$test.<br>";
 	}
 
