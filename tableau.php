a73c7263df191951e53f9ddb9bd967bd7007da85
Quentin Grau <q.grau00@gmail.com>
2017-03-08 13:37:41 +0100

    tout

---
+++ tableau.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+
+<?php  
+	
+	$arrayName = array('1' => janvier , fevrier );
+
+var_dump($arrayName);
+
+
+?>
+
+</body>
+</html>
