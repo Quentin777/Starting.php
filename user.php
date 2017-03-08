9ada98f94d33ad813d72f051eb6d9bef1aa532ba
Quentin Grau <q.grau00@gmail.com>
2017-03-08 16:11:17 +0100

    tout

--- user.php
+++ user.php
@@ -6,10 +6,10 @@
 </head>
 <body>
 	
-	<p> Bonjour, <?php echo htmlspecialchars($_GET['nom']); ?>.</p>
+	<p> Bonjour, <?php echo htmlspecialchars($_GET['Civilité']);($_GET['nom']); ?>.</p>
 	<p>Tu as <?php echo (int)$_GET['age']; ?> ans.</p>
 
-	<p> Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.</p>
+	<p> Bonjour, <?php echo htmlspecialchars($_POST['Civilité'])($_POST['nom']); ?>.</p>
 	<p>Tu as <?php echo (int)$_POST['age']; ?> ans.</p>
 
 </body>
