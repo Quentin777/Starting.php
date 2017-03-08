f638f0c66f30f5ed1ee301158876b3d01e6c29a6
Quentin Grau <q.grau00@gmail.com>
2017-03-08 11:46:51 +0100

    tout

--- fonction.php
+++ fonction.php
@@ -5,12 +5,46 @@
 	<link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 <body>
+
 <?php  
 	
-  function coucou() {
-  $resultat = true;
+  function exo1() {
+  	$result = true;
   	echo "$result";
   }
+?>
+
+
+<?php  
+	
+  function exo2(){
+  	$coucou = "yolo";
+  	echo $coucou;
+  }
+
+?>
+
+<?php  
+	
+  function exo3(){
+  	$a = "yolo";
+  	$b = "yolo"
+
+  	return $a.$b;
+  }
+
+?>
+
+
+<?php  
+	
+  function exo5(){
+  	$a = "yolo";
+  	$b = 6;
+
+  	return $a.$b;
+  }
+
 ?>
 
 </body>
