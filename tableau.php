b61f54984bad15e43c0508444a2565257ba4cbf3
Quentin Grau <q.grau00@gmail.com>
2017-03-09 12:00:14 +0100

    tout

--- tableau.php
+++ tableau.php
@@ -1,6 +1,17 @@
 <?php 
 
-$array = ("janvier", "fevrier", "mars", "avril", "mai" , "juin", "juillet","aout","septembre","octobre", "novembre" , "decembre");
+$mois = ('janvier', 
+		 'fevrier', 
+		 'mars', 
+		 'avril', 
+		 'mai' , 
+		 'juin', 
+		 'juillet',
+		 'aout',
+		 'septembre',
+		 'octobre', 
+		 'novembre' , 
+		 'decembre');
  ?>
 
 <!DOCTYPE html>
@@ -11,15 +22,15 @@ $array = ("janvier", "fevrier", "mars", "avril", "mai" , "juin", "juillet","aout
 </head>
 <body>
 
-<?php  //exo1 , 2
+<?php  //exo1 
 
-echo print_r($array);
+echo $mois[2];
 	
 ?>
 
-<?php // exo 5
+<?php // exo 2
 
-$tableau = array('nom du département' => Aisne, 'num du département' => 02, 'nom du département' => Nord, 'num du département' => 59, 'nom du département' =>Oise , 'num du département' => 60);
+echo $mois[2];
 
 ); 
 
