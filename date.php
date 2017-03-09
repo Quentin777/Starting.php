eec7efac302630f0a0640f8d67d38b87ef0efd3b
Quentin Grau <q.grau00@gmail.com>
2017-03-09 16:38:20 +0100

    tout

--- date.php
+++ date.php
@@ -13,11 +13,24 @@ echo strftime("%A %e %B %Y");
 <?php
 
 
-echo time();
+echo time().'<br>';
 
 // Secondes écoulées entre le 1er janvier 1970 et le 27 mai 2002 à midi
-echo mktime(15, 34, 12, 3, 2, 2016); 
+echo mktime(15, 34, 12, 3, 2, 2016).'<br>'; 
 
+
+$date1 = mktime(0,0,0,5,16,2016);
+$date2 = time();
+ 
+// On récupère la différence de timestamp entre les 2 précédents
+$nbJoursTimestamp = $date2 - $date1;
+ 
+// ** Pour convertir le timestamp (exprimé en secondes) en jours **
+// On sait que 1 heure = 60 secondes * 60 minutes et que 1 jour = 24 heures donc :
+$nbJours = $nbJoursTimestamp/86400; // 86 400 = 60*60*24
+ 
+echo "Nombre de jours : ".$nbJours;
+ 
 ?>
 
 </body>
