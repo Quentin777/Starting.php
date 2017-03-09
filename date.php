d108db3799dfa1e8b157155a872645c4b2bdcde9
Quentin Grau <q.grau00@gmail.com>
2017-03-09 15:27:26 +0100

    tout

---
+++ date.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<?php  
+echo date('d/m/Y h:i:s')
+
+?>
+</body>
+</html>
