c4375381678516ab4c06678247d925156f38d4fb
Quentin Grau <q.grau00@gmail.com>
2017-03-09 11:34:36 +0100

    tout

--- superglobal2.php
+++ superglobal2.php
@@ -24,8 +24,14 @@ $_SESSION['age'] = 24;
 </head>
 <body>
 
-	<p>
+	<p> <!-- crée un formulaire pour modier les cookie) -->
    Tu t'appelles <?php echo $_COOKIE['login'] = $_POST['login']; ?> et ton mdp est <?php echo $_COOKIE['mdp'] = $_POST['mdp']; ?> </p>
+
+ <form action="superglobal2.php" method="POST">
+ <label for="login"> <p>Login: <input type="text" name="login" /></p></label>
+ <label for="mdp"> <p>mots de passe: <input type="text" name="mdp" /></p></label>
+ <p><input type="submit" value="OK"></p>
+</form>
 
 </body>
 </html>
