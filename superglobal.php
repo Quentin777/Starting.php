61110f185e61faefedce240526dea79040fc009b
Quentin Grau <q.grau00@gmail.com>
2017-03-09 10:19:27 +0100

    tout

--- superglobal.php
+++ superglobal.php
@@ -1,8 +1,9 @@
 <?php
 
-// On démarre la session AVANT d'écrire du code HTML
-
 session_start();
+setcookie('login', 'login', time() + 365*24*3600, null, null, false, true); // On écrit un cookie
+
+setcookie('mdp', 'mdp', time() + 365*24*3600, null, null, false, true); // On écrit un autre 
 
 
 // On s'amuse à créer quelques variables de session dans $_SESSION
@@ -29,6 +30,17 @@ echo $_SERVER['REMOTE_ADDR'];
 echo $_SERVER ['SERVER_NAME'];
 echo $_SERVER ['HTTP_USER_AGENT'];
 ?>
+
+<form action="superglobal.php" method="GET">
+ <p>Login: <input type="text" name="login" /></p>
+ <p>mots de passe: <input type="text" name="mdp" /></p>
+ <SELECT name="Civilité" size="1">
+	<OPTION>Mr
+	<OPTION>Mme
+	<OPTION>Mademoiselle
+	</SELECT>
+ <p><input type="submit" value="OK"></p>
+</form>
 
 </body>
 </html>
