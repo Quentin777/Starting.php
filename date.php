2f8d07e5f0fce2b99293f01946bc6bc75e299781
Quentin Grau <q.grau00@gmail.com>
2017-03-09 16:04:10 +0100

    tout

--- date.php
+++ date.php
@@ -8,5 +8,17 @@
 <?php  
 echo strftime("%A %e %B %Y");
 ?>
+
+
+<?php
+
+
+echo time();
+
+// Secondes écoulées entre le 1er janvier 1970 et le 27 mai 2002 à midi
+echo mktime(15, 34, 12, 3, 2, 2016); 
+
+?>
+
 </body>
 </html>
