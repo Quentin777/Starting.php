a48e93075590b0550ff6151a433f7d2d504fe293
Quentin Grau <q.grau00@gmail.com>
2017-03-10 10:31:22 +0100

    correction

--- date.php
+++ date.php
@@ -34,31 +34,74 @@ echo "Nombre de jours : ".$nbJours;
 ?>
 
 <br><br>
+<?php  
+$calendrier = new DateTime('2007-06-1');
+$nombreDeJours = cal_days_in_month(CAL_GREGORIAN, 6, 2007);
+$numtour=0;
+
+switch ($calendrier->format('l')){
+    case 'Monday':
+        $index=0;
+        break;
+    case 'Tuesday':
+        $index=1;
+        break;
+    case 'Wednesday':
+        $index=2;
+        break;
+    case 'Thursday':
+        $index=3;
+        break;
+    case 'Friday':
+        $index=4;
+        break;
+    case 'Saturday':
+        $index=5;
+        break;
+    case 'Sunday':
+        $index=6;
+        break;
+}
+
+$numerosdumois = array();
+for ($i=0; $i < $index ; $i++) {
+    array_push($numerosdumois, ' ');
+}
+for ($numerojours=1; $numerojours <= $nombreDeJours ; $numerojours++) {
+    array_push($numerosdumois, $numerojours);
+}
+for ($i=0; $i < 6-$index ; $i++) {
+    array_push($numerosdumois, ' ');
+}
+
+
+?>
+<link rel="stylesheet" type="text/css" href="style/css/style.css">
 
 <table>
-	<tr colspan="7"> mois et l'année</tr>
-	<tr>
-		<td>Lun</td>	
-		<td>Mar</td>
-		<td>Mer</td>
-		<td>Jeu</td>
-		<td>Ven</td>
-		<td>Sam</td>
-		<td>Dim</td>
-	</tr>
-<?php  for ($semaine=0; $semaine <= 4; $semaine++){ ?>
-	<tr>
-		<td>coucou</td>	
-		<td>coucou</td>
-		<td>coucou</td>
-		<td>coucou</td>
-		<td>coucou</td>
-		<td>coucou</td>
-		<td>coucou</td>
-	</tr>	
-	<?php
-}
-?>
+    <tr>
+        <td colspan="7">mois et l'année</td>
+    </tr>    
+    <tr>
+        <td>Lu</td>
+        <td>Ma</td>
+        <td>Me</td>
+        <td>Je</td>
+        <td>Ve</td>
+        <td>Sa</td>
+        <td>Di</td>
+    </tr>
+
+    <?php
+    for ($semaine=0; $semaine <= 4; $semaine++) {
+        echo '<tr>';
+        for ($jour=0; $jour <= 6; $jour++) {
+            echo '<td>'.$numerosdumois[$numtour].'</td>';
+            $numtour++;
+        }
+        echo '</tr>';    
+    }    ?>
+
 </table>
 
 </body>
